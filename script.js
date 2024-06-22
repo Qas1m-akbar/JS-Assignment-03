d6ef79a8f35c754f444ca488f913ea1824a46bff
Qas1m-akbar <qasimktk901@gmail.com>
2024-06-22 10:14:01 -0700

    all tasks done

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-//METHOD 01: Index of //
+//METHOD 01: Index of --> This method is for to find the index number of the element in the following array//
 
 // let cars = ["Mehran", "Audi", "Suzuki", "Mercedes"]
 // console.log(cars.indexOf ("Mercedes"));
 
 
-// METHOD 02: POP() //
+// METHOD 02: POP() --> Only remove one element from end of the arrar at Once  //
 
 
 // let cars = [];
@@ -19,7 +19,7 @@
 
 
 
-// METHOD 03: Push() //
+// METHOD 03: Push() --> Add Element at the end of the array --> Add more than 2 arrays//
 
 // let cars = [];
 // cars[0] = "Mehran";
@@ -31,17 +31,51 @@
 // console.log(cars);
 
 
-// METHOD 04: shift() //
+// METHOD 04: shift()  --> This method is for to remove an Element from start of the array --> And can only we can remove single Element//
 
 // let cars = ["Mehran", "Audi", "Suzuki", "Mercedes"]
 // console.log(cars.shift());
+// console.log(cars);
 
 
 
-// METHOD 05: unshift() //
+// METHOD 05: unshift() --> This method is for add new element in start of the array --> And also we can add 2 or more new Elements //
 
 // let cars = ["Mehran", "Audi", "Suzuki", "Mercedes"];
-// console.log(cars.unshift());
+// cars.unshift("civic",2015,"New Model");
+// console.log(cars);
 
 
-// METHOD 06: Concat() //
+// METHOD 06: Concat() --> This Method can contact mean add two arrays in the third arrays  //
+
+//  let arr1 = [1,2,4,5]
+//  let arr2 = [3,5,8,9]
+//  let arr3 = arr1.concat(arr2);
+//  console.log(arr3);
+
+
+// METHOD 07: include() --> This Method can tell true or false work as a boolean --> if the element found in the include() then it will print "True" , otherwise print "False" //
+
+// let cars = ["Mehran", "Audi", "Suzuki", "Mercedes"]
+// console.log(cars.includes("Audi"));
+// console.log(cars);
+
+
+
+
+// METHOD 08: length() --> To  find the length of the array Elements//
+
+// let score =  [1,2,3,4,5,78,9,8,9,8];
+// console.log(score.length);
+
+
+
+//METHOD 09 //
+
+
+// let score = [10,20,30];
+// score[1] = 25;
+// console.log(score);
+
+
+
