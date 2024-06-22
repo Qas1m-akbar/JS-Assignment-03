9d5ecbcc7905ce276e6faae550f421c745a99a04
Qas1m-akbar <qasimktk901@gmail.com>
2024-06-22 09:48:43 -0700

    task 1to5 done

---
+++ script.js
@@ -0,0 +1,47 @@
+//METHOD 01: Index of //
+
+// let cars = ["Mehran", "Audi", "Suzuki", "Mercedes"]
+// console.log(cars.indexOf ("Mercedes"));
+
+
+// METHOD 02: POP() //
+
+
+// let cars = [];
+// cars[0] = "Mehran";
+// cars[1] = "Audi";
+// cars[2] = "Suzuki";
+// cars[3] = "Lembargini";
+// cars[4] = "CHR";
+// cars.pop();
+// console.log(cars);
+
+
+
+
+// METHOD 03: Push() //
+
+// let cars = [];
+// cars[0] = "Mehran";
+// cars[1] = "Audi";
+// cars[2] = "Suzuki";
+// cars[3] = "Lembargini";
+// cars[4] = "CHR";
+// cars.push("Grandi");
+// console.log(cars);
+
+
+// METHOD 04: shift() //
+
+// let cars = ["Mehran", "Audi", "Suzuki", "Mercedes"]
+// console.log(cars.shift());
+
+
+
+// METHOD 05: unshift() //
+
+// let cars = ["Mehran", "Audi", "Suzuki", "Mercedes"];
+// console.log(cars.unshift());
+
+
+// METHOD 06: Concat() //
